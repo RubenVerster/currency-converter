395d37eb9d00023d6345cee665850928f255940c
ruben.verster <ruben.verster@1-grid.com>
2021-08-25 23:38:14 +0200

    Documentation

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,17 @@ import { useState, useEffect } from 'react';
 import ConversionHistory from './components/ConversionHistory';
 import HistoricalData from './components/HistoricalData';
 
+//the base url for the API call
 const BASE_URL =
   'http://api.exchangeratesapi.io/v1/latest?access_key=d3b5401b496524bf3dc6143b8cc358b9';
 
+/**
+ *Component that nests all other components and passes the relative API data to child components
+ */
 const App = () => {
+  //function that fetches the conversion rates data from the API
   const retrieveCurrencies = async () => {
+    //here we try to do the API call
     try {
       const response = await axios.get(BASE_URL);
       const currencies = await Object.keys(response.data.rates);
@@ -18,6 +24,7 @@ const App = () => {
         await setCurrencyListOptions(currencies);
         await setExchangeRates(response.data.rates);
       }
+      //if the call fails, we display an error message
     } catch (error) {
       alert(
         'Error Retrieving Data From API. Might be that I used all my requests for the month... fuck...'
@@ -25,10 +32,14 @@ const App = () => {
     }
   };
 
+  //state hook that stores the exchange rates results from the API call
   const [exchangeRates, setExchangeRates] = useState<[]>([]);
+  //state hook that stores the different currency names for the select dropdown
   const [currencyListOptions, setCurrencyListOptions] = useState<string[]>([]);
+  //state hook that manages the history of the user's currency conversions
   const [conversionHistory, setConversionHistory] = useState([]);
 
+  //when this component mounts, we call the function to retrieve the exchange rates
   useEffect(() => {
     retrieveCurrencies();
   }, []);

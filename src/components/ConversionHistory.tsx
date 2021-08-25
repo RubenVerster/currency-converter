fcc85f6e7eec8a385001c6b2adc23c13a137f20d
ruben.verster <ruben.verster@1-grid.com>
2021-08-25 23:32:36 +0200

    Documentation and basic TS implemented

--- src/components/ConversionHistory.tsx
+++ src/components/ConversionHistory.tsx
@@ -1,6 +1,20 @@
-const ConversionHistory = ({ historyItems }: any) => {
-  const renderHistoryList = (historyItems: any) => {
+interface IHistoryItem {
+  amount: number;
+  from: string;
+  to: string;
+  converted: number;
+}
+
+/**
+ * component that manages rendering the UI for the user's currency conversion history
+ * @param historyItems props received from App.tsx
+ */
+//TS error fixed used for array that contains IHistoryItem array
+//@ts-ignore
+const ConversionHistory = ({ historyItems }) => {
+  const renderHistoryList = (historyItems: IHistoryItem[]) => {
     if (historyItems.length < 1) {
+      //if the user does not have any currency conversions in his history, we display a placeholder message instead
       return (
         <div className="pt-2 flex items-center content-center">
           <p className="text-gray-400 text-center p-6 h-full">
@@ -9,16 +23,17 @@ const ConversionHistory = ({ historyItems }: any) => {
         </div>
       );
     } else {
+      //if a user has a currency conversion in their history, map over the history array and generate a UI for each conversion's details
       return (
         <div className="text-center my-3">
-          {historyItems.map((item: any) => {
+          {historyItems.map(({ amount, from, to, converted }: IHistoryItem) => {
             return (
-              <p key={item}>
-                {item.amount}
-                <span className="font-bold px-1">{item.from}</span>
+              <p key={Math.random()}>
+                {amount}
+                <span className="font-bold px-1">{from}</span>
                 <span className="pr-1">=</span>
-                {item.converted}
-                <span className="font-bold px-1">{item.to}</span>
+                {converted}
+                <span className="font-bold px-1">{to}</span>
               </p>
             );
           })}
